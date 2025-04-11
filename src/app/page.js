2b9a26de468e8eecf9f0373fa4724ace0d2e2fda
Georgi Tsankov <multiartdelux@abv.bg>
2025-04-11 16:19:47 +0300

    Initialize Next.js project with Tailwind CSS setup

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+// src/app/page.js
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Добре дошли в Tailwind CSS! dadadad</h1>
+    </div>
+  );
+}
